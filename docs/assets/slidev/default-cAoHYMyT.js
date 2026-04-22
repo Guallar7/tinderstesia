27332e4efc0f7e8df0150bf20c4b78f35acf426c
Guallar7 <dreamdavidai@gmail.com>
2026-04-22 12:53:16 +0200

    Update docs folder with corrected build paths
    Build now without base path so that docs/ folder serves correctly from GitHub Pages root.

---
+++ docs/assets/slidev/default-cAoHYMyT.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-CH7c4Z0o.js";import{_ as r}from"../index-Mjka-L6u.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
